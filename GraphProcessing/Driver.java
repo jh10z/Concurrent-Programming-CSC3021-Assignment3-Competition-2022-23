6c5f98cf036c2798a5f53de2224de4fc552dad15
40259391 <kho07@qub.ac.uk>
2022-10-10 14:01:58 +0100

    Reset the Driver.java class for submission

--- GraphProcessing/Driver.java
+++ GraphProcessing/Driver.java
@@ -19,180 +19,178 @@ import uk.ac.qub.csc3021.graph.*;
 // Main class with main() method. Performs the PageRank computation until
 // convergence is reached.
 class Driver {
-    public static void main(String args[]) {
-		if(args.length < 5) {
-			System.err.println( "Usage: java Driver algorithm num-threads outputfile format inputfiles..." );
-			return;
-		}
+    public static void main( String args[] ) {
+	if( args.length < 5 ) {
+	    System.err.println( "Usage: java Driver algorithm num-threads outputfile format inputfiles..." );
+	    return;
+	}
 
-		String algorithm = args[0];
-		int num_threads = Integer.parseInt(args[1]);
-		String outputFile = args[2];
-		String format = args[3];
-		String inputFileCOO = null;
-		String inputFileCSR = null;
-		String inputFileCSC = null;
-		for(int i = 4; i < args.length; i++) {
-			String ext = args[i].substring(args[i].lastIndexOf(".") + 1);
-			if(ext.equals("csc")) {
-				inputFileCSC = args[i];
-			}
-			else if(ext.equals("csr")) {
-				inputFileCSR = args[i];
-			}
-			else if(ext.equals("coo")) {
-				inputFileCOO = args[i];
-			}
-			else if(ext.equals("csc-csr")) {
-				inputFileCSC = args[i];
-				inputFileCSR = args[i];
-			} else {
-				System.err.println( "argument " + i + " has unrecognized filename extension \"" + ext + "\"" );
-				System.err.println( "Note that files must be decompressed before calling this program" );
-				System.err.println( "Usage: java Driver algorithm num-threads outputfile format inputfiles..." );
-				return;
-			}
-		}
+	String algorithm = args[0];
+	int num_threads = Integer.parseInt( args[1] );
+	String outputFile = args[2];
+	String format = args[3];
+	String inputFileCOO = null;
+	String inputFileCSR = null;
+	String inputFileCSC = null;
+	for( int i=4; i < args.length; ++i ) {
+	    String ext = args[i].substring( args[i].lastIndexOf( "." ) + 1 );
+	    if( ext.equals( "csc" ) )
+		inputFileCSC = args[i];
+	    else if( ext.equals( "csr" ) )
+		inputFileCSR = args[i];
+	    else if( ext.equals( "coo" ) )
+		inputFileCOO = args[i];
+	    else if( ext.equals( "csc-csr" ) ) {
+		inputFileCSC = args[i];
+		inputFileCSR = args[i];
+	    } else {
+		System.err.println( "argument " + i + " has unrecognized filename extension \"" + ext + "\"" );
+		System.err.println( "Note that files must be decompressed before calling this program" );
+		System.err.println( "Usage: java Driver algorithm num-threads outputfile format inputfiles..." );
+		return;
+	    }
+	}
 
-		// Tell us what you're doing
-		System.err.println( "Format: " + format);
-		System.err.println( "Input file CSR: " + inputFileCSR);
-		System.err.println( "Input file CSC: " + inputFileCSC);
-		System.err.println( "Input file COO: " + inputFileCOO);
-		System.err.println( "Algorithm: " + algorithm);
-		System.err.println( "Number of threads: " + num_threads);
-		System.err.println( "Output file: " + outputFile);
+	// Tell us what you're doing
+	System.err.println( "Format: " + format );
+	System.err.println( "Input file CSR: " + inputFileCSR );
+	System.err.println( "Input file CSC: " + inputFileCSC );
+	System.err.println( "Input file COO: " + inputFileCOO );
+	System.err.println( "Algorithm: " + algorithm );
+	System.err.println( "Number of threads: " + num_threads );
+	System.err.println( "Output file: " + outputFile );
 
-		long tm_start = System.nanoTime();
+	long tm_start = System.nanoTime();
 
-		SparseMatrix matrix;
+	SparseMatrix matrix;
 
-		// Step 1. Read in the file
-		if(format.equalsIgnoreCase("CSR")) {
-			matrix = new SparseMatrixCSR( inputFileCSR);
-		} else if(format.equalsIgnoreCase("CSC") || format.equalsIgnoreCase("PARCSC")) {
-			matrix = new SparseMatrixCSC( inputFileCSC );
-		} else if(format.equalsIgnoreCase("COO")) {
-			matrix = new SparseMatrixCOO( inputFileCOO );
-		} else if(format.equalsIgnoreCase("ICHOOSE") ) {
-			// Uncomment what you want to use
-			matrix = new SparseMatrixCOO( inputFileCOO );
-			// matrix = new SparseMatrixCSR( inputFileCSR );
-			// matrix = new SparseMatrixCSC( inputFileCSC );
-		} else {
-			System.err.println("Unknown format '" + format + "'");
-			System.exit(43); // Kattis
-			return; // silence compiler errors
-		}
+	// Step 1. Read in the file
+	if( format.equalsIgnoreCase( "CSR" ) ) {
+	    matrix = new SparseMatrixCSR( inputFileCSR );
+	} else if( format.equalsIgnoreCase( "CSC" )
+		   || format.equalsIgnoreCase( "PARCSC" ) ) {
+	    matrix = new SparseMatrixCSC( inputFileCSC );
+	} else if( format.equalsIgnoreCase( "COO" ) ) {
+	    matrix = new SparseMatrixCOO( inputFileCOO );
+	} else if( format.equalsIgnoreCase( "ICHOOSE" ) ) {
+	    // Pick any you want.
+	    // matrix = new SparseMatrixCOO( inputFileCOO );
+	    matrix = new SparseMatrixCSR( inputFileCSR );
+	    // matrix = new SparseMatrixCSC( inputFileCSC );
+	} else {
+	    System.err.println( "Unknown format '" + format + "'" );
+            System.exit(43); // Kattis
+	    return; // silence compiler errors
+	}
 
-		double tm_input = (double)(System.nanoTime() - tm_start) * 1e-9;
-		System.err.println("Reading input: " + tm_input + " seconds");
+	double tm_input = (double)(System.nanoTime() - tm_start) * 1e-9;
+	System.err.println( "Reading input: " + tm_input + " seconds" );
+	tm_start = System.nanoTime();
+
+	// What facilities for parallel execution do we have?
+	// Options:
+	// - ParallelContextSingleThread: fully implemented
+	// - ParallelContextSimple: needs to be completed by yourself when
+	//   asked for in the assignment brief.
+	// if( format.equalsIgnoreCase( "PARCSC" ) )
+	// ParallelContextHolder.set( new ParallelContextSimple(num_threads) );
+	// else
+	ParallelContextHolder.set( new ParallelContextSingleThread() );
+
+	try {
+	    if( algorithm.equalsIgnoreCase( "PR" ) ) {
+		// Step 2. Calculate PageRank values for the graph
+		double PR[] = PageRank.compute( matrix );
+
+		double tm_total = (double)(System.nanoTime() - tm_start) * 1e-9;
+		System.err.println( "PageRank: total time: " + tm_total + " seconds" );
 		tm_start = System.nanoTime();
 
-		// What facilities for parallel execution do we have?
-		// Options:
-		// - ParallelContextSingleThread: fully implemented
-		// - ParallelContextSimple: needs to be completed by yourself when
-		//   asked for in the assignment brief.
-		// if( format.equalsIgnoreCase( "PARCSC" ) )
-		// ParallelContextHolder.set( new ParallelContextSimple(num_threads) );
-		// else
-		ParallelContextHolder.set(new ParallelContextSingleThread());
+		// Step 3. Dump PageRank values to file
+		writeToFile( outputFile, PR );
 
-		try {
-			if(algorithm.equalsIgnoreCase("PR")) {
-			// Step 2. Calculate PageRank values for the graph
-			double PR[] = PageRank.compute(matrix);
+		double tm_write = (double)(System.nanoTime() - tm_start) * 1e-9;
+		System.err.println( "Writing file: " + tm_write + " seconds" );
+	    } else if( algorithm.equalsIgnoreCase( "CC" ) ) {
+		// Step 2. Calculate connected components of the graph
+		int CC[] = ConnectedComponents.compute( matrix );
 
-			double tm_total = (double)(System.nanoTime() - tm_start) * 1e-9;
-			System.err.println("PageRank: total time: " + tm_total + " seconds");
-			tm_start = System.nanoTime();
+		double tm_total = (double)(System.nanoTime() - tm_start) * 1e-9;
+		System.err.println( "Connected Components: total time: " + tm_total + " seconds" );
+		tm_start = System.nanoTime();
 
-			// Step 3. Dump PageRank values to file
-			writeToFile(outputFile, PR);
+		// Step 3. Dump component sizes to file
+		writeToFile( outputFile, CC );
 
-			double tm_write = (double)(System.nanoTime() - tm_start) * 1e-9;
-			System.err.println("Writing file: " + tm_write + " seconds");
-			} else if(algorithm.equalsIgnoreCase("CC")) {
-			// Step 2. Calculate connected components of the graph
-				int CC[] = ConnectedComponents.compute(matrix);
+		double tm_write = (double)(System.nanoTime() - tm_start) * 1e-9;
+		System.err.println( "Writing file: " + tm_write + " seconds" );
+	    } else if( algorithm.equalsIgnoreCase( "DS" ) ) {
+		// Step 2. Calculate connected components of the graph
+		int CC[] = DisjointSetCC.compute( matrix );
 
-				double tm_total = (double)(System.nanoTime() - tm_start) * 1e-9;
-				System.err.println("Connected Components: total time: " + tm_total + " seconds");
-				tm_start = System.nanoTime();
+		double tm_total = (double)(System.nanoTime() - tm_start) * 1e-9;
+		System.err.println( "Disjoint Set: total time: " + tm_total + " seconds" );
+		tm_start = System.nanoTime();
 
-				// Step 3. Dump component sizes to file
-				writeToFile(outputFile, CC);
+		// Step 3. Dump component sizes to file
+		writeToFile( outputFile, CC );
 
-				double tm_write = (double)(System.nanoTime() - tm_start) * 1e-9;
-				System.err.println("Writing file: " + tm_write + " seconds");
-			} else if(algorithm.equalsIgnoreCase("DS")) {
-			// Step 2. Calculate connected components of the graph
-				int CC[] = DisjointSetCC.compute( matrix );
-
-				double tm_total = (double)(System.nanoTime() - tm_start) * 1e-9;
-				System.err.println("Disjoint Set: total time: " + tm_total + " seconds");
-				tm_start = System.nanoTime();
-
-				// Step 3. Dump component sizes to file
-				writeToFile(outputFile, CC);
-
-				double tm_write = (double)(System.nanoTime() - tm_start) * 1e-9;
-				System.err.println("Writing file: " + tm_write + " seconds");
-			} else {
-				System.err.println("Unknown algorithm '" + algorithm + "'");
-				return;
-			}
-		} finally {
-			ParallelContextHolder.get().terminate();
-		}
-		System.err.println("All done");
+		double tm_write = (double)(System.nanoTime() - tm_start) * 1e-9;
+		System.err.println( "Writing file: " + tm_write + " seconds" );
+	    } else {
+		System.err.println( "Unknown algorithm '" + algorithm + "'" );
+		return;
+	    }
+	} finally {
+	    ParallelContextHolder.get().terminate();
+	}
+	System.err.println( "All done" );
     }
 
-    static void writeToFile(String file, double[] v) {
-		try {
-			OutputStreamWriter os = new OutputStreamWriter(new FileOutputStream(file), "UTF-8" );
-			BufferedWriter wr = new BufferedWriter(os);
-			writeToBuffer(wr, v);
-		} catch(FileNotFoundException e) {
-			System.err.println( "File not found: " + e );
-			return;
-		} catch(UnsupportedEncodingException e) {
-			System.err.println( "Unsupported encoding exception: " + e );
-			return;
-		} catch(Exception e) {
-			System.err.println( "Exception: " + e );
-			return;
-		}
+    static void writeToFile( String file, double[] v ) {
+	try {
+	    OutputStreamWriter os
+		= new OutputStreamWriter( new FileOutputStream( file ), "UTF-8" );
+	    BufferedWriter wr = new BufferedWriter( os );
+	    writeToBuffer( wr, v );
+	} catch( FileNotFoundException e ) {
+	    System.err.println( "File not found: " + e );
+	    return;
+	} catch( UnsupportedEncodingException e ) {
+	    System.err.println( "Unsupported encoding exception: " + e );
+	    return;
+	} catch( Exception e ) {
+	    System.err.println( "Exception: " + e );
+	    return;
+	}
     }
     static void writeToBuffer( BufferedWriter buf, double[] v ) {
-		PrintWriter out = new PrintWriter( buf );
-		for(int i = 0; i < v.length; i++) {
-			out.println(i + " " + v[i]);
-		}
-		out.close();
+	PrintWriter out = new PrintWriter( buf );
+	for( int i=0; i < v.length; ++i )
+	    out.println( i + " " + v[i] );
+	out.close();
     }
     static void writeToFile( String file, int[] v ) {
-		try {
-			OutputStreamWriter os = new OutputStreamWriter(new FileOutputStream( file ), "UTF-8");
-			BufferedWriter wr = new BufferedWriter(os);
-			writeToBuffer(wr, v);
-		} catch(FileNotFoundException e) {
-			System.err.println("File not found: " + e);
-			return;
-		} catch(UnsupportedEncodingException e) {
-			System.err.println( "Unsupported encoding exception: " + e );
-			return;
-		} catch(Exception e) {
-			System.err.println( "Exception: " + e );
-			return;
-		}
+	try {
+	    OutputStreamWriter os
+		= new OutputStreamWriter( new FileOutputStream( file ), "UTF-8" );
+	    BufferedWriter wr = new BufferedWriter( os );
+	    writeToBuffer( wr, v );
+	} catch( FileNotFoundException e ) {
+	    System.err.println( "File not found: " + e );
+	    return;
+	} catch( UnsupportedEncodingException e ) {
+	    System.err.println( "Unsupported encoding exception: " + e );
+	    return;
+	} catch( Exception e ) {
+	    System.err.println( "Exception: " + e );
+	    return;
+	}
     }
-    static void writeToBuffer(BufferedWriter buf, int[] v) {
-		PrintWriter out = new PrintWriter( buf );
-		for(int i = 0; i < v.length; i++) {
-			out.println(i + " " + v[i]);
-		}
-		out.close();
+    static void writeToBuffer( BufferedWriter buf, int[] v ) {
+	PrintWriter out = new PrintWriter( buf );
+	for( int i=0; i < v.length; ++i )
+	    out.println( i + " " + v[i] );
+	out.close();
     }
 }
