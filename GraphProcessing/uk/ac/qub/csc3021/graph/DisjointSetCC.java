defc0431b52e98fd5633615ff260c2b46a7bfa6d
40259391 <kho07@qub.ac.uk>
2022-12-11 16:23:04 +0000

    Disjoint Attempt

--- GraphProcessing/uk/ac/qub/csc3021/graph/DisjointSetCC.java
+++ GraphProcessing/uk/ac/qub/csc3021/graph/DisjointSetCC.java
@@ -6,82 +6,127 @@ import java.util.concurrent.atomic.AtomicIntegerArray;
 // This algorithm only works correctly for undirected graphs
 public class DisjointSetCC {
     private static class DSCCRelax implements Relax {
-	DSCCRelax( AtomicIntegerArray parent_ ) {
-	    //...
-	}
+		DSCCRelax(AtomicIntegerArray parent_) {
+			this.parent = parent_;
+		}
 
-	public void relax( int src, int dst ) {
-	    union( src, dst );
-	}
+		public void relax(int src, int dst) {
+			union(src, dst);
+		}
 
-	public int find( int x ) {
-	    //...
-		return 0; //Self-Note: Added in for now
-	}
-	
-	private boolean sameSet( int x, int y ) {
-		//...
-		return false; //Self-Note: Added in for now
-	}
+		public int find(int x) {
+			int u = x;
+			while(u != parent.get(u)) {
+				u = parent.get(u);
+			}
+			return u;
+		}
 
-	private boolean union( int x, int y ) {
-		//...
-		return false; //Self-Note: Added in for now
-	}
+		private boolean sameSet(int x, int y) {
+			int u = x;
+			int v = y;
+			while(true) {
+				u = find(x);
+				v = find(v);
+				if(u == v) {
+					return true;
+				}
+				if(u == parent.get(u)) {
+					return false;
+				}
+			}
+		}
 
-	// Variable declarations
-	private AtomicIntegerArray parent;
-    };
+		private boolean union(int x, int y) {
+			int u = x;
+			int v = y;
 
-    public static int[] compute( SparseMatrix matrix ) {
-	long tm_start = System.nanoTime();
+			while(true) {
+				u = find(x);
+				v = find(v);
+				if(u == v) {
+					return true;
+				}
+				if(u < v) {
+					if(CAS(parent.get(u), v, u)) {
+						return false;
+					}
+				}
+				else if (u == v) {
+					return true;
+				}
+				else if (CAS(parent.get(v), v, u)) {
+					return false;
+				}
+			}
+		}
 
-	final int n = matrix.getNumVertices();
-	final AtomicIntegerArray parent = new AtomicIntegerArray( n );
-	final boolean verbose = true;
+		private synchronized boolean CAS(int x, int y, int z) {
+			if(x == y) {
+				x = z;
+				return true;
+			} else {
+				return false;
+			}
+		}
 
-	for( int i=0; i < n; ++i ) {
-	    // Each vertex is a set on their own
-	    //...
-	}
+		// Variable declarations
+		private AtomicIntegerArray parent;
+	};
 
-	DSCCRelax DSCCrelax = new DSCCRelax( parent );
+    public static int[] compute(SparseMatrix matrix) {
+		long tm_start = System.nanoTime();
 
-	double tm_init = (double)(System.nanoTime() - tm_start) * 1e-9;
-	System.err.println( "Initialisation: " + tm_init + " seconds" );
-	tm_start = System.nanoTime();
+		final int n = matrix.getNumVertices();
+		final AtomicIntegerArray parent = new AtomicIntegerArray(n);
+		final boolean verbose = true;
 
-	ParallelContext context = ParallelContextHolder.get();
+		for(int i = 0; i < n; ++i) {
+			parent.set(i, i);
+		}
 
-	// 1. Make pass over graph
-	context.edgemap( matrix, DSCCrelax );
+		DSCCRelax DSCCrelax = new DSCCRelax(parent);
 
-	double tm_step = (double)(System.nanoTime() - tm_start) * 1e-9;
-	if( verbose )
-	    System.err.println( "processing time=" + tm_step + " seconds" );
-	tm_start = System.nanoTime();
+		double tm_init = (double)(System.nanoTime() - tm_start) * 1e-9;
+		System.err.println("Initialisation: " + tm_init + " seconds");
+		tm_start = System.nanoTime();
 
-	// Post-process the labels
+		ParallelContext context = ParallelContextHolder.get();
 
-	// 1. Count number of components
-	//    and map component IDs to narrow domain
-	int ncc = 0;
-	int remap[] = new int[n];
-	for( int i=0; i < n; ++i )
-	    if( DSCCrelax.find( i ) == i )
-		remap[i] = ncc++;
+		// 1. Make pass over graph
+		context.edgemap( matrix, DSCCrelax );
 
-	if( verbose )
-	    System.err.println( "Number of components: " + ncc );
+		double tm_step = (double)(System.nanoTime() - tm_start) * 1e-9;
+		if(verbose) {
+			System.err.println("processing time=" + tm_step + " seconds");
+		}
+		tm_start = System.nanoTime();
 
-	// 2. Calculate size of each component
-	int sizes[] = new int[ncc];
-	for( int i=0; i < n; ++i )
-	    ++sizes[remap[DSCCrelax.find( i )]];
+		// Post-process the labels
 
-	if( verbose )
-	    System.err.println( "DisjointSetCC: " + ncc + " components" );
+		// 1. Count number of components
+		//    and map component IDs to narrow domain
+		int ncc = 0;
+		int remap[] = new int[n];
+		for (int i = 0; i < n; ++i)
+			if (DSCCrelax.find(i) == i) {
+				remap[i] = ncc++;
+			}
 
-	return sizes;
+		if(verbose) {
+			System.err.println("Number of components: " + ncc);
+		}
+
+		// 2. Calculate size of each component
+		int sizes[] = new int[ncc];
+		for(int i = 0; i < n; ++i) {
+			++sizes[remap[DSCCrelax.find(i)]];
+		}
+
+		if(verbose) {
+			System.err.println("DisjointSetCC: " + ncc + " components");
+		}
+
+		return sizes;
     }
 }
--- GraphProcessing/uk/ac/qub/csc3021/graph/Relax.java
+++ GraphProcessing/uk/ac/qub/csc3021/graph/Relax.java
@@ -3,5 +3,5 @@ package uk.ac.qub.csc3021.graph;
 // Functional interface that describes the operation performed when visiting
 // an edge.
 interface Relax {
-    public void relax( int source, int destination );
+    public void relax(int source, int destination);
 }
